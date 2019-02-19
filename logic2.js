d50f55cf0eba96fd7a45436fa354bc9e5b17c922
jesberman <jesberman@gmail.com>
2019-02-19 17:12:31 -0500

    number-buttons-and-equal-button-commented-out

--- logic2.js
+++ logic2.js
@@ -1,5 +1,6 @@
 var sum;
 var calculation;
+var sign;
 
 function myFunction() {
     var a = document.getElementById("number-1").value;
@@ -17,12 +18,53 @@ function myFunction() {
         sum = a - b;
         alert(sum);
     }
-     if (c === "/") {
+    if (c === "/") {
         sum = a / b;
         alert(sum);
-    } 
+    }
     if (c === "*") {
         sum = a * b;
         alert(sum);
     }
+}
+
+function pressPlus() {
+    var a = document.getElementById("number-1").value;
+    var b = document.getElementById("number-2").value;
+
+    a = Number(a);
+    b = Number(b);
+
+    document.getElementById("view-pane").innerHTML = a+b;
+}
+
+function pressMinus() {
+    var a = document.getElementById("number-1").value;
+    var b = document.getElementById("number-2").value;
+
+    a = Number(a);
+    b = Number(b);
+
+    document.getElementById("view-pane").innerHTML = a-b;
+}
+
+function pressTimes() {
+    var a = document.getElementById("number-1").value;
+    var b = document.getElementById("number-2").value;
+
+    a = Number(a);
+    b = Number(b);
+
+    document.getElementById("view-pane").innerHTML = a*b;
+}
+
+
+function pressDivide() {
+    var a = document.getElementById("number-1").value;
+    var b = document.getElementById("number-2").value;
+
+    a = Number(a);
+    b = Number(b);
+
+    document.getElementById("view-pane").innerHTML = a/b;
 }
