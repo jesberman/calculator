e4ae2f1904d9bf3a9031b33dec4e2c5ea4debbb3
jesberman <jesberman@gmail.com>
2019-02-19 17:14:44 -0500

    commented-out-items-deleted

--- logic2.js
+++ logic2.js
@@ -2,31 +2,6 @@ var sum;
 var calculation;
 var sign;
 
-function myFunction() {
-    var a = document.getElementById("number-1").value;
-    var b = document.getElementById("number-2").value;
-    var c = document.getElementById("operator").value;
-
-    a = Number(a);
-    b = Number(b);
-
-    if (c === "+") {
-        sum = a + b;
-        alert(sum);
-    }
-    if (c === "-") {
-        sum = a - b;
-        alert(sum);
-    }
-    if (c === "/") {
-        sum = a / b;
-        alert(sum);
-    }
-    if (c === "*") {
-        sum = a * b;
-        alert(sum);
-    }
-}
 
 function pressPlus() {
     var a = document.getElementById("number-1").value;
